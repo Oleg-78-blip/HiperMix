829587538f4d3a294824c2184cdbc062a3acb318
Oleg Gor <147436932+Oleg-78-blip@users.noreply.github.com>
2024-11-19 22:04:16 +0200

    add carousel

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ btn.addEventListener('click', clickBtn);
 function clickBtn() {
   btn.classList.toggle('active');
   img.classList.toggle('active');
-  console.log('hhhhhhhh')
 }
 
 $(document).ready(function(){
@@ -27,3 +26,30 @@ $(document).ready(function(){
 });
 
 console.log('hellodthet')
+
+// Отримати всі посилання навігації
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Поточний URL
+const currentUrl = window.location.pathname;
+
+// Додати клас "active" до відповідного посилання
+navLinks.forEach(link => {
+  if (link.getAttribute('href') === currentUrl) {
+    link.classList.add('active');
+  }
+});
+
+ let currentIndex = 0;
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    function moveSlide(direction) {
+      currentIndex += direction;
+      if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+      } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+      }
+      slides.style.transform = 'translateX(' + (-currentIndex * 100) + '%)';
+    }
